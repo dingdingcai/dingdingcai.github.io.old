ce59da6e8387902666444981f8aead558d0bcac2
Dingding <caidingding815@foxmail.com>
2017-09-21 15:58:16 +0300

    Delete submit.sh

--- submit.sh
+++
@@ -1,5 +0,0 @@
-cd /home/dingding/ding2/github/dingdingcai.github.io
-git add *
-git commit -m "modify index.html"
-#git push -u origin master
-git push https://dingdingcai:mbyd798791565@github.com/dingdingcai/dingdingcai.github.io.git
